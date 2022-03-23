1c78a7952832a19fe77bf61c2be04db9a1736b66
AshleyZG <gsarazh@gmail.com>
2022-03-23 16:55:32 -0400

    sideview and wrapper widget

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ import {
 } from '@jupyterlab/notebook';
 import { IDisposable, DisposableDelegate } from '@lumino/disposable';
 import { ToolbarButton } from '@jupyterlab/apputils';
+import { PanelLayout } from '@lumino/widgets';
 
 import { CodeCell } from '@jupyterlab/cells';
 import {Cell} from '@jupyterlab/cells';
@@ -23,7 +24,7 @@ import {
     ReadonlyPartialJSONValue
   } from '@lumino/coreutils';
 
-import { Wrapper } from './wrapper';
+import { Wrapper, SideViewWidget, SideViewModel } from './wrapper';
 
 
 function newOnMetadataChanged (cell: CodeCell){
@@ -68,108 +69,148 @@ function newOnMetadataChanged (cell: CodeCell){
 /**
  * A notebook widget extension that adds a button to the toolbar.
  */
- export class BranchButtonExtension
+export class BranchButtonExtension
  implements DocumentRegistry.IWidgetExtension<NotebookPanel, INotebookModel>
-{
- /**
-  * Create a new extension for the notebook panel widget.
-  *
-  * @param panel Notebook panel
-  * @param context Notebook context
-  * @returns Disposable on the added button
-  */
+    {
+    /**
+     * Create a new extension for the notebook panel widget.
+     *
+     * @param panel Notebook panel
+     * @param context Notebook context
+     * @returns Disposable on the added button
+     */
 
- wrapperList: Wrapper[] = [];
+    // wrapperList: Wrapper[] = [];
+    sideViewWidget: SideViewWidget | undefined;
+    createNew(
+        panel: NotebookPanel,
+        context: DocumentRegistry.IContext<INotebookModel>
+        ): IDisposable {
 
- createNew(
-   panel: NotebookPanel,
-   context: DocumentRegistry.IContext<INotebookModel>
- ): IDisposable {
-   const callback = () => {
-     this.createWrapper(panel);
-   };
-   const forkCallback = () => {
-     this.fork(panel);
-   }
+        // add a widget to the right of notebook panel
+        // and set notebook width to 80%, check css
+        var sideViewModel = new SideViewModel();
+        var sideView = new SideViewWidget(sideViewModel);
+        (panel.layout as PanelLayout).addWidget(sideView);
+        (panel.layout as PanelLayout).widgets[2].addClass('my-notebook-content-panel');
+        this.sideViewWidget = sideView;
 
-   const button = new ToolbarButton({
-     className: 'create-wrapper-button',
-     label: 'Create Wrapper',
-     onClick: callback,
-     tooltip: 'Create A Wrapper',
-   });
+        const callback = () => {
+            this.createWrapper(panel);
+        };
+        const forkCallback = () => {
+            this.fork(panel);
+        }
+        const hideCallback = () => {
+            this.hide(panel);
+        }
 
-   const forkButton = new ToolbarButton({
-     className: 'fork-branch',
-     label: 'Fork',
-     onClick: forkCallback,
-     tooltip: 'Fork A Branch',
-   })
+        const button = new ToolbarButton({
+            className: 'create-wrapper-button',
+            label: 'Create Wrapper',
+            onClick: callback,
+            tooltip: 'Create A Wrapper',
+        });
 
+        const forkButton = new ToolbarButton({
+            className: 'fork-branch',
+            label: 'Fork',
+            onClick: forkCallback,
+            tooltip: 'Fork A Branch',
+        })
 
-
-   panel.toolbar.insertItem(10, 'clearOutputs', button);
-   panel.toolbar.insertItem(11, 'fork', forkButton);
-   return new DisposableDelegate(() => {
-     button.dispose();
-     forkButton.dispose();
-   });
- }
-
- createWrapper(panel: NotebookPanel){
-  console.log('create wrapper');
-
-  // get active cell information 
-  var activeCell = panel.content.activeCell;
-  var activeCellIndex = panel.content.activeCellIndex;
-
-  // reset metadata.changed fn
-  activeCell?.model.metadata.changed.connect(newOnMetadataChanged(panel.content.widgets[activeCellIndex] as CodeCell));
-
-  // create a wrapper model
-  var wrapper = new Wrapper(this.wrapperList.length);
-  this.wrapperList.push(wrapper);
-  wrapper.insertCell(activeCell!);
-
-  // add inWrapper attribute to metadata
-  activeCell!.model.sharedModel.setMetadata({inWrapper: true,
-    wrapperID: wrapper.id});
-  // activeCell!.model.sharedModel.setMetadata({});
+        const hideButton = new ToolbarButton({
+            className: 'hide-branch',
+            label: 'Hide',
+            onClick: hideCallback,
+            tooltip: 'Hide A Branch',
+        })
 
 
-}
+        panel.toolbar.insertItem(10, 'clearOutputs', button);
+        panel.toolbar.insertItem(11, 'fork', forkButton);
+        panel.toolbar.insertItem(12, 'hide', hideButton);
+        return new DisposableDelegate(() => {
+            button.dispose();
+            forkButton.dispose();
+            hideButton.dispose();
+        });
+    }
 
-fork(panel: NotebookPanel){
-  console.log('fork');
-  var activeCellIndex = panel.content.activeCellIndex;
+    createWrapper(panel: NotebookPanel){
+        console.log('create wrapper');
 
-  // copy paste a cell
-  var activeCell = panel.content.activeCell;
-  var activeYCell = panel.model!.sharedModel.cells[activeCellIndex];
-  var newYCell = YCodeCell.create();
-  panel.model?.sharedModel.insertCell(activeCellIndex+1, newYCell);
-  newYCell.setSource(activeYCell.getSource());
-  var newCell = panel.content.widgets[activeCellIndex+1];
-  newCell.model.metadata.changed.connect(newOnMetadataChanged(newCell as CodeCell));
-  
-  // get wrapper
-  var wrapperID = activeCell?.model.sharedModel.getMetadata().wrapperID;
-  var wrapper = this.wrapperList[wrapperID];
+        // get active cell information 
+        var activeCell = panel.content.activeCell;
+        var activeCellIndex = panel.content.activeCellIndex;
 
-  // insert new cell to wrapper
-  wrapper.insertCell(newCell);
+        // reset metadata.changed fn
+        activeCell?.model.metadata.changed.connect(newOnMetadataChanged(panel.content.widgets[activeCellIndex] as CodeCell));
 
-  // update metadata
-  var newWidth: string = (100/wrapper.length).toString()+'%';
-  newCell.model.metadata.set('inWrapper', true);
-  newCell.model.metadata.set('wrapperID', wrapper.id);
-  console.log(newCell.model.sharedModel.getMetadata());
-  wrapper.cellList.forEach((cell: Cell) => {
-    cell.model.metadata.set('width', newWidth);
-  })
+        // create a wrapper model
+        // var wrapper = new Wrapper(this.wrapperList.length);
+        // var wrapper = new Wrapper(this.sideViewWidget!.model.wrappers.length);
+        // this.wrapperList.push(wrapper);
+        var wrapper: Wrapper = this.sideViewWidget!.model.createWrapper();
+        wrapper.insertCell(activeCell!);
+        
+        // this.sideViewWidget?.addWrapper(wrapper);
 
-}
+        // add inWrapper attribute to metadata
+        activeCell!.model.metadata.set('inWrapper', true);
+        activeCell!.model.metadata.set('wrapperID', wrapper.id);
 
+    }
+
+    fork(panel: NotebookPanel){
+        console.log('fork');
+        var activeCellIndex = panel.content.activeCellIndex;
+
+        // copy paste a cell
+        var activeCell = panel.content.activeCell;
+        var activeYCell = panel.model!.sharedModel.cells[activeCellIndex];
+        var newYCell = YCodeCell.create();
+        panel.model?.sharedModel.insertCell(activeCellIndex+1, newYCell);
+        newYCell.setSource(activeYCell.getSource());
+        var newCell = panel.content.widgets[activeCellIndex+1];
+        newCell.model.metadata.changed.connect(newOnMetadataChanged(newCell as CodeCell));
+        
+        // get wrapper
+        var wrapperID = activeCell?.model.sharedModel.getMetadata().wrapperID;
+        var wrapper = this.sideViewWidget!.model.wrappers[wrapperID];
+
+        // insert new cell to wrapper
+        wrapper.insertCell(newCell);
+
+        // update metadata
+        var newWidth: string = (100/wrapper.length).toString()+'%';
+        newCell.model.metadata.set('inWrapper', true);
+        newCell.model.metadata.set('wrapperID', wrapper.id);
+        console.log(newCell.model.sharedModel.getMetadata());
+        wrapper.cellList.forEach((cell: Cell) => {
+            cell.model.metadata.set('width', newWidth);
+        })
+
+    }
+
+    /**
+     * This function if for user to hide branches. 
+     * @param panel 
+     */
+    hide(panel: NotebookPanel){
+        console.log("Not Implemented");
+        var activeCell = panel.content.activeCell;
+        // var activeCellID = activeCell?.model.id;
+
+        // get wrapper
+        var wrapperID = activeCell?.model.sharedModel.getMetadata().wrapperID;
+        var wrapper = this.sideViewWidget!.model.wrappers[wrapperID];
+
+        console.log(wrapper);
+        // wrapper.
+        debugger;
+
+    }
 }
 
 /**
--- src/wrapper.tsx
+++ src/wrapper.tsx
@@ -1,22 +1,93 @@
 import {Cell} from '@jupyterlab/cells';
+import { VDomRenderer, VDomModel, UseSignal } from '@jupyterlab/apputils';
 
+import React from 'react';
 class Wrapper {
-  id: number;
-  cellList: Cell[] = [];
+    id: number;
+    cellList: Cell[] = [];
 
-  constructor(id: number){
-    this.id = id;
-  }
+    constructor(id: number){
+        this.id = id;
+    }
 
-  insertCell(newCell: Cell){
-    this.cellList.push(newCell);
-  }
-  updateCellOrder(){}
-  removeCell(){}
+    insertCell(newCell: Cell){
+        this.cellList.push(newCell);
+    }
+    updateCellOrder(){}
+    removeCell(){}
 
-  get length(){
-    return this.cellList.length;
-  }
+    get length(){
+        return this.cellList.length;
+    }
 }
 
-export {Wrapper};
+
+interface WrapperProps{
+    wrapper: Wrapper;
+};
+interface WrapperState{};
+class WrapperWidget extends React.Component<WrapperProps, WrapperState>{
+    constructor(props:any){
+        super(props);
+        
+    }
+    render(): React.ReactNode {
+        return <div className='wrapper'>
+            {this.props.wrapper.id}
+        </div>
+    }
+}
+
+class SideViewModel extends VDomModel {
+    wrappers: Wrapper[] = [];
+    constructor(){
+        super();
+    }
+
+    createWrapper(): Wrapper{
+        var wrapper = new Wrapper(this.wrappers.length);
+        this.wrappers.push(wrapper);
+        this.stateChanged.emit();
+        return wrapper;
+    }
+
+    pushCellToWrapper(){
+
+    }
+
+}
+
+
+class SideViewWidget extends VDomRenderer<SideViewModel> {
+
+    sideViewRef: React.RefObject<HTMLElement>;
+
+    constructor(model: SideViewModel) {
+        super(model);
+        this.addClass('jp-ReactWidget');
+        this.addClass('sideview');
+
+        this.sideViewRef =  React.createRef();
+    }
+
+
+    render(): JSX.Element {
+        return <div> 
+            <UseSignal signal={this.model.stateChanged} >
+                {(): JSX.Element => {
+                    return <div>{this.model.wrappers.length}
+                        {this.model.wrappers.map((wrapper: Wrapper) => {
+                            return <WrapperWidget wrapper={wrapper}/>
+                        })}
+                    </div>
+                }}
+            </UseSignal>
+
+        </div> 
+
+    }
+}
+
+
+
+export {Wrapper, SideViewWidget, SideViewModel};
