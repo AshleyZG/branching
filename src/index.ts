e64abbbc2fce8881e2375c6adcdef03d2eb5a40f
AshleyZG <gsarazh@gmail.com>
2022-03-30 15:31:43 -0400

    override CellCode execute method

--- src/index.ts
+++ src/index.ts
@@ -12,12 +12,19 @@ import { ToolbarButton } from '@jupyterlab/apputils';
 import { PanelLayout } from '@lumino/widgets';
 
 import { CodeCell } from '@jupyterlab/cells';
-// import {Cell} from '@jupyterlab/cells';
+import { ISessionContext } from '@jupyterlab/apputils';
+import { Kernel, KernelMessage } from '@jupyterlab/services';
+import {
+  OutputArea,
+} from '@jupyterlab/outputarea';
 
 import {
-  // ISharedCell,
   YCodeCell
 } from '@jupyterlab/shared-models';
+
+import {
+  JSONObject,
+} from '@lumino/coreutils';
 
 import { IObservableJSON, IObservableMap } from '@jupyterlab/observables';
 import {
@@ -56,6 +63,7 @@ function newOnMetadataChanged (cell: CodeCell){
   }
   return fn
 }
+
 
 /**
  * The plugin registration information.
@@ -96,7 +104,8 @@ export class BranchButtonExtension
         this.sideViewWidget = sideView;
 
 
-
+        // set CodeCell execute method
+        CodeCell['execute'] = this.execute;
 
         const callback = () => {
             this.createWrapper(panel);
@@ -220,6 +229,105 @@ export class BranchButtonExtension
       `;
       //  '';
       panel.context.sessionContext.session?.kernel?.requestExecute({code: script}, true);
+    }
+
+    async execute(
+      cell: CodeCell,
+      sessionContext: ISessionContext,
+      metadata?: JSONObject    
+    ): Promise<KernelMessage.IExecuteReplyMsg | void> {
+      const model = cell.model;
+      // const code = model.value.text;
+      const code = `print('hello')`;
+      if (!code.trim() || !sessionContext.session?.kernel) {
+        model.clearExecution();
+        return;
+      }
+      const cellId = { cellId: model.id };
+      metadata = {
+        ...model.metadata.toJSON(),
+        ...metadata,
+        ...cellId
+      };
+      const { recordTiming } = metadata;
+      model.clearExecution();
+      cell.outputHidden = false;
+      cell.setPrompt('*');
+      model.trusted = true;
+      let future:
+        | Kernel.IFuture<
+            KernelMessage.IExecuteRequestMsg,
+            KernelMessage.IExecuteReplyMsg
+          >
+        | undefined;
+      try {
+        const msgPromise = OutputArea.execute(
+          code,
+          cell.outputArea,
+          sessionContext,
+          metadata
+        );
+        // cell.outputArea.future assigned synchronously in `execute`
+        if (recordTiming) {
+          const recordTimingHook = (msg: KernelMessage.IIOPubMessage) => {
+            let label: string;
+            switch (msg.header.msg_type) {
+              case 'status':
+                label = `status.${
+                  (msg as KernelMessage.IStatusMsg).content.execution_state
+                }`;
+                break;
+              case 'execute_input':
+                label = 'execute_input';
+                break;
+              default:
+                return true;
+            }
+            // If the data is missing, estimate it to now
+            // Date was added in 5.1: https://jupyter-client.readthedocs.io/en/stable/messaging.html#message-header
+            const value = msg.header.date || new Date().toISOString();
+            const timingInfo: any = Object.assign(
+              {},
+              model.metadata.get('execution')
+            );
+            timingInfo[`iopub.${label}`] = value;
+            model.metadata.set('execution', timingInfo);
+            return true;
+          };
+          cell.outputArea.future.registerMessageHook(recordTimingHook);
+        } else {
+          model.metadata.delete('execution');
+        }
+        // Save this execution's future so we can compare in the catch below.
+        future = cell.outputArea.future;
+        const msg = (await msgPromise)!;
+        model.executionCount = msg.content.execution_count;
+        if (recordTiming) {
+          const timingInfo = Object.assign(
+            {},
+            model.metadata.get('execution') as any
+          );
+          const started = msg.metadata.started as string;
+          // Started is not in the API, but metadata IPyKernel sends
+          if (started) {
+            timingInfo['shell.execute_reply.started'] = started;
+          }
+          // Per above, the 5.0 spec does not assume date, so we estimate is required
+          const finished = msg.header.date as string;
+          timingInfo['shell.execute_reply'] =
+            finished || new Date().toISOString();
+          model.metadata.set('execution', timingInfo);
+        }
+        return msg;
+      } catch (e) {
+        // If we started executing, and the cell is still indicating this
+        // execution, clear the prompt.
+        if (future && !cell.isDisposed && cell.outputArea.future === future) {
+          cell.setPrompt('');
+        }
+        throw e;
+      }
+    
     }
 }
 
